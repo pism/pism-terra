c58a80f598f7f0f5a370b2285c8fdde01e1a1b39
Joseph H Kennedy <me@jhkennedy.org>
2025-11-25 22:02:04 -0900

    CDS API credentials helper

--- pism_terra/__main__.py
+++ pism_terra/__main__.py
@@ -3,8 +3,11 @@ Pism-terra entrypoint dispatcher for PISM-Cloud.
 """
 
 import argparse
+import os
 import sys
+import warnings
 from importlib.metadata import entry_points
+from pathlib import Path
 
 
 def main():
@@ -26,6 +29,15 @@ def main():
     )
 
     args, unknowns = parser.parse_known_args()
+
+    cds_api_url = os.environ.get("CDS_API_URL")
+    cds_api_key = os.environ.get("CDS_API_KEY")
+    if (cds_api_file := Path.home() / ".cdsapirc").exists():
+        warnings.warn(
+            "CDS API credentials provided in both environment variables and the `~/.cdsapirc` file. Preferring file."
+        )
+    else:
+        cds_api_file.write_text(f"url: {cds_api_url}\nkey: {cds_api_key}\n")
 
     eps = entry_points(group="console_scripts")
     (process_entry_point,) = {process for process in eps if process.name == args.process}
