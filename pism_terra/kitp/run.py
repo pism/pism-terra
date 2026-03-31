a335ae2660024bd9c0b58fa7c7062d2889859d50
Andy Aschwanden <andy.aschwanden@gmail.com>
2026-03-31 13:53:15 -0800

    Added ocean_file to run_single

--- pism_terra/kitp/run.py
+++ pism_terra/kitp/run.py
@@ -413,6 +413,7 @@ def run_single():
             "energy.bedrock_thermal.file": row["heatflux_file"],
             "grid.file": row["grid_file"],
             "atmosphere.given.file": row["climate_file"],
+            "ocean.given.file": row["ocean_file"],
             "surface.pdd.std_dev.file": row["climate_file"],
             "surface.debm_simple.std_dev.file": row["climate_file"],
             "surface.debm_simple.albedo_input.file": row["climate_file"],
